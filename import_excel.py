c446cb96ad22a6751839a4ce3f838fc26ef4f6c9
Marianne Fritsch <marianne.fritsch@etu.minesparis.psl.eu>
2025-07-24 19:52:22 +0200

    actualisation du site avec l'excel

--- import_excel.py
+++ import_excel.py
@@ -3,6 +3,7 @@ import numpy as np
 import sqlite3
 from config import DB_NAME, EXCEL_PATH
 
+
 def import_excel_to_numpy_and_insert():
     # 1. Lecture Excel
     df = pd.read_excel(EXCEL_PATH)
@@ -19,8 +20,10 @@ def import_excel_to_numpy_and_insert():
 
     for row in data_array:
         titre = str(row[0]).strip()
-        auteur = str(row[1]).strip()        
-        edition = str(row[2]).strip() if len(row) > 2 else None  # dépend de l'ordre dans Excel
+        auteur = str(row[1]).strip()
+        edition = (
+            str(row[2]).strip() if len(row) > 2 else None
+        )  # dépend de l'ordre dans Excel
         remarque = str(row[3]).strip() if len(row) > 3 else None
         genre = str(row[4]).strip()
         resume = str(row[5]).strip()
@@ -28,12 +31,20 @@ def import_excel_to_numpy_and_insert():
         # Force status = 'disponible'
         statut = "disponible"
 
-        # Utilise les bons noms de colonnes !
-        cursor.execute("SELECT COUNT(*) FROM books WHERE title = ? AND author = ?", (titre, auteur))
-        if cursor.fetchone()[0] == 0:
+        # Si le livre existe, on le met à jour
+        cursor.execute(
+            "SELECT * FROM books WHERE title = ? AND author = ?", (titre, auteur)
+        )
+        existing = cursor.fetchone()
+        if existing:
             cursor.execute(
-                 "INSERT INTO books (title, author, status, edition, remarque, genre, resume) VALUES (?, ?, ?, ?, ?, ?, ?)",
-                (titre, auteur, statut, edition, remarque, genre, resume)
+                "UPDATE books SET edition = ?, remarque = ?, genre = ?, resume = ? WHERE title = ? AND author = ?",
+                (edition, remarque, genre, resume, titre, auteur),
+            )
+        else:
+            cursor.execute(
+                "INSERT INTO books (title, author, status, edition, remarque, genre, resume) VALUES (?, ?, ?, ?, ?, ?, ?)",
+                (titre, auteur, statut, edition, remarque, genre, resume),
             )
 
             print(f"> Livre ajouté : {titre} - {auteur}")
